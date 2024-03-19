7014f94a0f3e9217c684ac662c9710343cac76ff
renzivan243 <107743082+renzivan243@users.noreply.github.com>
2024-03-20 01:56:01 +0800

    fix backend

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json()); // To parse JSON bodies
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "10000mb" }));
+app.use(express.urlencoded({ limit: "10000mb", extended: true }));
 
 // Multer configuration for handling multipart/form-data with no file size limit
 const storage = multer.memoryStorage(); // Using memory storage
